13140e8e8a3e0b10e93c93a8a3392255363671ca
Popoola-Sinaayo <olusegunpopoola4real@gmail.com>
2020-10-13 01:37:43 -0700

    Update

--- Essential_Glams/urls.py
+++ Essential_Glams/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('success', views.success, name='success'),
     path('register_mail', views.register_mail, name="register_mail"),
     path('comment-handler', views.comment_handler, name="comment_handler"),
-    path('product', views.product, name="product"),
     path('product-video', views.product_video, name="video"),
     path('test', views.test, name="test"),
     path('confirm', views.confirm, name="confirm")
